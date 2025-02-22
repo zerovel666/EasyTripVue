5dc29051aa9277e2f2465dd4df276d5b3159504a
zerovel666 <zerovel060606@gmail.com>
2025-02-22 17:04:54 +0500

    init

---
+++ src/Pages/Main.vue
@@ -0,0 +1,13 @@
+<template>
+    <TopBar />
+</template>
+
+<script setup>
+import TopBar from '@/components/Layouts/TopBar.vue';
+
+
+</script>
+
+<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,19 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import Main from './Pages/Main.vue'
+
+const routes = [
+    { path: '/', component: Main }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
