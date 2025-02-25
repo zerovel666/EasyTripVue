f21b1e072f0036d86385dbb8c36023b7604bab66
zerovel666 <zerovel060606@gmail.com>
2025-02-25 21:51:00 +0500

    add button all cities and add computed visibleCards

--- src/components/MainPagesLayouts/Cities.vue
+++ src/components/MainPagesLayouts/Cities.vue
@@ -2,17 +2,23 @@
     <div class="container">
         <h2>Исследуй мир с EasyTrip</h2>
         <p>
-            Погрузись в увлекательное путешествие по {{countTrip}} городам вместе с EasyTrip! Открой для себя новые места, окунись
-            в
-            атмосферу удивительных культур и создай незабываемые воспоминания. Мир ждет тебя – отправляйся в дорогу!
+            Погрузись в увлекательное путешествие по {{ countTrip }} городам вместе с EasyTrip!
+            Открой для себя новые места, окунись в атмосферу удивительных культур и создай незабываемые воспоминания.
+            Мир ждет тебя – отправляйся в дорогу!
         </p>
         <div class="content">
-            <div class="card" v-for="(city,index) in cities" :key="index" :style="{backgroundImage: `url(${city.image_path})`}">
+            <div class="card" 
+                 v-for="(city, index) in visibleCities" 
+                 :key="index" 
+                 :style="{ backgroundImage: `url(${city.image_path})` }">
                 <div class="card_infos">
                     <p>{{ city.country_name }}</p>
                     <p class="count_trip">{{ city.count_trip }} городов для посещения</p>
                 </div>
             </div>
+        </div>
+        <div id="buttonCont">
+            <button>Посмотреть все города</button>
         </div>
     </div>
 </template>
@@ -20,11 +26,12 @@
 <script setup>
 import { API_URL } from '@/config';
 import axios from 'axios';
-import { onMounted, ref,defineEmits } from 'vue';
+import { onMounted, ref, computed, defineEmits } from 'vue';
 
 const cities = ref([]);
-const countTrip = ref(null)
+const countTrip = ref(null);
 const emit = defineEmits(['updateCountTrip']);
+
 const getCities = async () => {
     const response = await axios.get(API_URL + "/country/countByname");
     cities.value = response.data;
@@ -33,8 +40,15 @@ const getCities = async () => {
     emit('updateCountTrip', countTrip.value);
 };
 
+const visibleCities = computed(() => {
+    const count = cities.value.length;
+    const maxCount = count - (count % 3);
+    return cities.value.slice(0, maxCount);
+});
+
 onMounted(getCities);
 </script>
+
 
 <style scoped>
 h2{
@@ -108,6 +122,29 @@ h2{
     .content { 
         grid-template-columns: repeat(3, 1fr);
     }
+}
+
+#buttonCont{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 30px;
+    flex-direction: column;
+}
+#buttonCont button{
+    width: 25%;
+    height: 50px;
+    font-size: 20px;
+    border-radius: 10px;
+    border: none;
+    background-color: #02BF8C;
+    color: white;
+    cursor: pointer;
+    transition: transform 0.3s ease; 
+    cursor: pointer;    
+}
+#buttonCont button:hover {
+    transform: scale(1.05); 
 }
 
 </style>
